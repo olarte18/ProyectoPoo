f2072e4e8ea6f1121d7a0e0511316ea61e14b5f5
olarte18 <alejolarte18@gmail.com>
2023-06-13 21:48:28 -0500

    se implementan todos los archivos restantes y se hace un intento de optimizacion de codigo reduciendo algunas lineas de codigo

--- controlador/Controlador.java
+++ controlador/Controlador.java
@@ -19,13 +19,16 @@ public class Controlador implements ActionListener
     private VentanaPrincipal venPrin;
     private Laboratorio lab;
 
+    //metodos
+    private void mostrarMensajeError(String mensaje) {
+    JOptionPane.showMessageDialog(null, mensaje);
+    }
     //Constructor
     public Controlador(VentanaPrincipal pVenPrin, Laboratorio pLab)
     {
         this.venPrin = pVenPrin;
         this.lab=pLab;
         this.venPrin.miPanelOperaciones.agregarOyentesBotones(this);
-        this.venPrin.miDialogoLaboratorio.agregarOyentesBotones1(this);
     }
     //sobreescritura del metodo actionPerformed
     @Override
@@ -34,70 +37,84 @@ public class Controlador implements ActionListener
         String comando = ae.getActionCommand();
 
     //se evalua el comando    
+    
 
    if(comando.equals("ver")){
 
         //se obtienen los indices de los combobox
 
-        int indx1=venPrin.miPanelDatos.getLaboratorioid();
-        int indx2=venPrin.miPanelDatos.getFuncionid();
+        int indiceLaboratorio = venPrin.miPanelDatos.getLaboratorioid();
+        int indiceFuncion = venPrin.miPanelDatos.getFuncionid();
 
+        if (indiceLaboratorio == 0 || indiceFuncion == 0) {
+        mostrarMensajeError("No ha seleccionado ningún elemento");
+        return;
+        }
         //se evaluan los indices
 
-        switch(indx1)
+        switch(indiceLaboratorio)
             {case 0:
                 //se muestra un mensaje de error
-                JOptionPane.showMessageDialog(null, "No ha seleccionado ningun elemento" );
                 break;
             //se evalua el segundo indice
             case 1:
-                switch(indx2)
+                switch(indiceFuncion)
                     {case 0:
                         JOptionPane.showMessageDialog(null, "No ha seleccionado ningun elemento" );
                         break;
                     case 1:
-                        //se crea un icono
+                        //se crea un icono y se le asigna un valor segun su indice
                         ImageIcon icono2 =new ImageIcon("imgs/Mesadefuerzas.png");
                         venPrin.crearDialogoLaboratorio();
                         venPrin.miDialogoLaboratorio.setIcono2(icono2);
                         break;
                     case 2:
+                        //se crea un icono y se le asigna un valor segun su indice
+                        ImageIcon icono =new ImageIcon("imgs/materialesi2.png");
                         venPrin.crearDialogoLaboratorio();
+                        venPrin.miDialogoLaboratorio.setIcono2(icono);
                         break;
             }
                 break;
             case 2:
-                switch(indx2)
+                switch(indiceFuncion)
                     {case 0:
+                        //se muestra un mensaje de error
                         JOptionPane.showMessageDialog(null, "No ha seleccionado ningun elemento" );
                         break;
                     case 1:
+                        //se crea un icono y se le asigna un valor segun su indice
                         ImageIcon icono2 =new ImageIcon("imgs/i182.png");
                         venPrin.crearDialogoLaboratorio();
                         venPrin.miDialogoLaboratorio.setIcono2(icono2);
                         break;
                     case 2:
+                        //se crea un icono y se le asigna un valor segun su indice
+                        ImageIcon icono =new ImageIcon("imgs/i18°2 Materiales.png");
                         venPrin.crearDialogoLaboratorio();
+                        venPrin.miDialogoLaboratorio.setIcono2(icono);
                         break;
             }
                 break;
             case 3:
-                switch(indx2)
+                switch(indiceFuncion)
                     {case 0:
-                        JOptionPane.showMessageDialog(null, "No ha seleccionado ningun elemento" );
+                        //se muestra un mensaje de error
                         break;
                     case 1:
-                        ImageIcon icono2 =new ImageIcon("");
+                        ImageIcon icono2 =new ImageIcon("img/i19.png");
                         venPrin.crearDialogoLaboratorio();
                         venPrin.miDialogoLaboratorio.setIcono2(icono2);
                         break;
                     case 2:
+                        ImageIcon icono =new ImageIcon("imgs/materialesi19 (2).png");
                         venPrin.crearDialogoLaboratorio();
+                        venPrin.miDialogoLaboratorio.setIcono2(icono);
                         break;
                     }
                 break;
             case 4:
-                switch(indx2)
+                switch(indiceFuncion)
                     {case 0:
                         JOptionPane.showMessageDialog(null, "No ha seleccionado ningun elemento" );
                         break;
@@ -107,12 +124,14 @@ public class Controlador implements ActionListener
                         venPrin.miDialogoLaboratorio.setIcono2(icono2);
                         break;
                     case 2:
+                        ImageIcon icono =new ImageIcon("imgs/materialesi20.png");
                         venPrin.crearDialogoLaboratorio();
+                        venPrin.miDialogoLaboratorio.setIcono2(icono);
                         break;
                     }
                 break;
             case 5:
-                switch(indx2)
+                switch(indiceFuncion)
                     {case 0:
                         JOptionPane.showMessageDialog(null, "No ha seleccionado ningun elemento" );
                         break;
@@ -122,15 +141,19 @@ public class Controlador implements ActionListener
                         venPrin.miDialogoLaboratorio.setIcono2(icono2);
                         break;
                     case 2:
-
+                        ImageIcon icono =new ImageIcon("imgs/materialesi22.png");
+                        venPrin.crearDialogoLaboratorio();
+                        venPrin.miDialogoLaboratorio.setIcono2(icono);
                         break;
              }   
                 break;}
              
     }
+    
  else if (comando.equals("salir")) {
     System.exit(0);
 
 }
+
 }
 }
